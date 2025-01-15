927db64d5b065f3f8b12e76ce189aa4d557b8770
Luc Patiny <luc@patiny.com>
2025-01-15 13:38:50 +0100

    chore: remove test files

--- frontend/src/test/DebugTempState.jsx
+++
@@ -1,8 +0,0 @@
-import { useSignals } from '@preact/signals-react/runtime';
-import { state } from '../getState.jsx';
-
-export function DebugTempState() {
-  useSignals();
-
-  return <pre>{JSON.stringify(state.temp, null, 2)}</pre>;
-}
