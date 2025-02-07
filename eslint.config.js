d8b50335b8290556a22745e9e757c0fd809ad40a
Luc Patiny <luc@patiny.com>
2025-02-07 16:24:31 +0100

    chore: cleanup eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,8 @@
-import { config } from 'eslint-config-zakodium';
+import { config, globals } from 'eslint-config-zakodium';
 import js from 'eslint-config-zakodium/js';
 import jsdoc from 'eslint-config-zakodium/jsdoc';
 import react from 'eslint-config-zakodium/react';
 import unicorn from 'eslint-config-zakodium/unicorn';
-import globals from 'globals';
 
 export default config(
   ...js,
@@ -13,17 +12,11 @@ export default config(
     // Global ignore patterns.
     ignores: ['**/dist'],
   },
-  // Apply TypeScript config on the whole project.
   {
-    // Apply Adonis v5 config only to the api.
-    files: ['api/**'],
-  },
-  {
-    // Apply React config only to the frontend.
     files: ['backend/**'],
     languageOptions: {
       globals: {
-        ...globals.node,
+        ...globals.nodeBuiltin,
       },
     },
   },
